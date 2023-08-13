152b69298a73c7a59e29d0a24cca613708a9e8b7
Mila-U <88654285+Mila-U@users.noreply.github.com>
2023-08-13 21:15:28 +0300

    initial commit

---
+++ src/main/java/com/example/springkafkapoc/SpringKafkaProofOfConceptApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaProofOfConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaProofOfConceptApplication.class, args);
+	}
+
+}
